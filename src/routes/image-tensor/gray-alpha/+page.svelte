8108da98127a4e2639cd4ae93aef26270a594ff9
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-05-28 21:07:08 +0900

    tf.fillでグレー画像を描く

---
+++ src/routes/image-tensor/gray-alpha/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import * as tf from "@tensorflow/tfjs"
+  import { onMount } from "svelte"
+
+  let canvas: HTMLCanvasElement
+
+  onMount(async () => {
+    const grayAlpha = tf.fill<tf.Rank.R3>([200, 200, 4], 0.5)
+
+    await tf.browser.toPixels(grayAlpha, canvas)
+    console.log("tensor used", tf.memory().numTensors)
+    grayAlpha.dispose()
+    console.log("tensor cleaned", tf.memory().numTensors)
+  })
+</script>
+
+<canvas width="200" height="200" bind:this={canvas} />
