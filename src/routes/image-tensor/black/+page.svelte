964036e54ef7c2eb6c97b0cc4c8141edfd524eaa
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-05-28 21:06:55 +0900

    tf.zerosで真っ黒画像を描く

---
+++ src/routes/image-tensor/black/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import * as tf from "@tensorflow/tfjs"
+  import { onMount } from "svelte"
+
+  let canvas: HTMLCanvasElement
+
+  onMount(async () => {
+    /** [height, width, color_dimension] */
+    const black = tf.zeros<tf.Rank.R3>([100, 200, 3])
+
+    await tf.browser.toPixels(black, canvas)
+    console.log("tensor used", tf.memory().numTensors)
+    black.dispose()
+    console.log("tensor cleaned", tf.memory().numTensors)
+  })
+</script>
+
+<canvas width="200" height="100" bind:this={canvas} />
